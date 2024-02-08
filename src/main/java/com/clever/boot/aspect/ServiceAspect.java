dcae180210d69db60cf26f1e9aaa9123652744a4
Ann_Mikhaliova <annmikhaleva35@gmail.com>
2024-02-08 14:54:16 +0300

    Add AOP

---
+++ src/main/java/com/clever/boot/aspect/ServiceAspect.java
@@ -0,0 +1,38 @@
+package com.clever.boot.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@Slf4j
+public class ServiceAspect {
+
+    @Pointcut(value = "execution(* com.clever.boot.service.HouseService.*(..))")
+    public void pointCutMethod() {
+    }
+
+    @Before("pointCutMethod()")
+    public void beforeCallAtMethod(JoinPoint jp) {
+        String args = Arrays.stream(jp.getArgs())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        log.info("before: {}, args=[{}]", jp.toShortString(), args);
+    }
+
+    @AfterReturning(pointcut="pointCutMethod()", returning="retVal")
+    public void afterReturningCallAt(JoinPoint jp,Object retVal) {
+        log.info("after: {}, result: {}", jp.toShortString(), retVal.toString());
+
+    }
+
+    @AfterThrowing(pointcut = "pointCutMethod()", throwing = "exception")
+    public void afterThrowingHouseService(Throwable exception) {
+        log.info("afterThrowingHouseService: exception message {}",exception.getMessage());
+    }
+}
